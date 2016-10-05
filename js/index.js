1cf8353c182da4f1fc38f1bf9b55fc88777323c6
Emma Simon <EmmaMaySimon@gmail.com>
2016-10-05 19:04:32 -0400

    Refactor JS to be more readable

--- js/index.js
+++ js/index.js
@@ -1,22 +1,26 @@
 (function() {
   'use strict';
 
-  // Initialize
   window.addEventListener('load', function() {
     var sections = document.getElementsByClassName('section-header');
-    for (var i = 0; i < sections.length; i++) {
-      sections[i].addEventListener('click', function(event) {
-        var section = event.target.parentElement;
-        var content = section.getElementsByClassName('content');
+    var i;
 
-        for (var i = 0; i < content.length; i++) {
-          if (content[i].clientHeight) {
-            content[i].style.maxHeight = 0;
-          } else {
-            content[i].style.maxHeight = content[i].scrollHeight + 'px';
-          }
+    function expand(event) {
+      var section = event.target.parentElement;
+      var content = section.getElementsByClassName('content');
+      var j;
+
+      for (j = 0; j < content.length; j++) {
+        if (content[j].clientHeight) {
+          content[j].style.maxHeight = 0;
+        } else {
+          content[j].style.maxHeight = content[j].scrollHeight + 'px';
         }
-      });
+      }
+    }
+
+    for (i = 0; i < sections.length; i++) {
+      sections[i].addEventListener('click', expand);
     }
   });
 })();
