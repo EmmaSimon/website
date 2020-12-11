7bc8d6ff135b545944fd7889c3354641750f0ecb
Emma Simon <emmamaysimon@gmail.com>
2020-12-11 04:19:35 +0000

    Add knit to served sites

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(jellyfish.software)
+domains=(knit.computer)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="emmamaysimon@gmail.com" # Adding a valid address is strongly recommended
